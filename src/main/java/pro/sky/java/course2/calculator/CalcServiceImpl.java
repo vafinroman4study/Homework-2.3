7ae3fd21241f789a88c08eb949202843c7bf2ed9
roman <romanvafin92@gmail.com>
2023-03-19 22:06:40 +0300

    ДЗ на проверку

--- src/main/java/pro/sky/java/course2/calculator/CalcServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/CalcServiceImpl.java
@@ -10,4 +10,26 @@ public class CalcServiceImpl implements CalcService {
     public String hello() {
         return "Решение по ДЗ урока «Вводное в Maven и Spring Boot + web»";
     }
+
+    @Override
+    public String helloCalc() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @Override
+    public int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public double division(int num1, int num2) {
+        return (float) num1 / num2;
+    }
+
+
 }
